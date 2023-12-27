f04fb20b5398cc3923bb71963b3b4c5d6815a7b7
barakadanny <barakadan421@gmail.com>
2023-12-28 02:29:22 +0300

    refresh method used

--- main.py
+++ main.py
@@ -23,8 +23,11 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    # Detect collision between Snake and Food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
