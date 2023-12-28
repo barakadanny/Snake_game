f72de6005d607ec4900adc11a7e716417d8b3ed2
barakadanny <barakadan421@gmail.com>
2023-12-28 03:29:17 +0300

    update main

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ while game_is_on:
     # Detect collision between Snake and Food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
 
     # Detect collision between Snake and the wall
