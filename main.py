7ed662eb0ef1d0aa76c8d581aa1c20753dc2dfe2
barakadanny <barakadan421@gmail.com>
2023-12-28 03:36:17 +0300

    collision with tail detected

--- main.py
+++ main.py
@@ -38,5 +38,13 @@ while game_is_on:
         scoreboard.game_over()
         game_is_on = False
 
+    # Detect collision between Snake and the tail
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+
 
 screen.exitonclick()
